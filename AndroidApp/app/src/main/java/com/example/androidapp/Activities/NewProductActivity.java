eb9790b752d6807d02a63296aaee509b9c946248
Anhduy-git <anhduydp123@gmail.com>
2022-03-10 11:04:19 +0700

    update selected type spinner

--- AndroidApp/app/src/main/java/com/example/androidapp/Activities/NewProductActivity.java
+++ AndroidApp/app/src/main/java/com/example/androidapp/Activities/NewProductActivity.java
@@ -1,16 +1,68 @@
 package com.example.androidapp.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
+import com.example.androidapp.Data.ProductAttribute.ProductAttribute;
+import com.example.androidapp.Data.ProductAttribute.ProductAttributeCategoryAdapter;
+import com.example.androidapp.Data.ProductAttribute.ProductAttributeViewModel;
 import com.example.androidapp.R;
 
-public class NewProductActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+public class NewProductActivity extends AppCompatActivity {
+    private ProductAttributeViewModel productAttributeViewModel;
+    private Spinner spinner;
+    private Button btnBack;
+    private ProductAttributeCategoryAdapter productAttributeCategoryAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_product);
+
+        spinner = findViewById(R.id.type_list);
+        btnBack = findViewById(R.id.btn_back);
+        productAttributeCategoryAdapter = new ProductAttributeCategoryAdapter(this, R.layout.item_selected_spinner, getListCategory());
+        spinner.setAdapter(productAttributeCategoryAdapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        //button back
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+
+
+    }
+
+    private List<ProductAttribute> getListCategory() {
+        //view model
+        productAttributeViewModel = new ViewModelProvider(this).get(ProductAttributeViewModel.class);
+        List<ProductAttribute> attributeLst = productAttributeViewModel.getAllProductAttribute();
+        return attributeLst;
     }
 }
