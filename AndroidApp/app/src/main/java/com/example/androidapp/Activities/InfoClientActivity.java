446aed65b64c313815b32f40a0b8661bdc005a14
Nguyễn Trần Anh Duy <84486806+Anhduy-git@users.noreply.github.com>
2022-04-22 10:32:08 +0700

    update splash screen (#35)

--- AndroidApp/app/src/main/java/com/example/androidapp/Activities/InfoClientActivity.java
+++ AndroidApp/app/src/main/java/com/example/androidapp/Activities/InfoClientActivity.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -22,13 +23,28 @@ import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.androidapp.Data.ClientData.ClientViewModel;
+import com.example.androidapp.Data.ProductData.Product;
+import com.example.androidapp.Data.ProductData.ProductViewModel;
+import com.example.androidapp.Data.ProductDetailData.ProductDetail;
+import com.example.androidapp.Data.ProductDetailData.ProductDetailViewModel;
+import com.example.androidapp.Data.ProductType.ProductType;
+import com.example.androidapp.Data.ProductType.ProductTypeViewModel;
+import com.example.androidapp.HelperClass.PriceQuantity;
+import com.example.androidapp.HelperClass.PriceQuantityAdapter;
+import com.example.androidapp.HelperClass.PriceQuantityItem;
+import com.example.androidapp.HelperClass.ProductAttributeItem;
 import com.example.androidapp.R;
+import com.google.android.flexbox.FlexboxLayout;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class InfoClientActivity extends AppCompatActivity {
     public static final String EXTRA_CLIENT =
@@ -171,4 +187,187 @@ public class InfoClientActivity extends AppCompatActivity {
             }
     );
 
+    public static class AddQuantityPriceProductActivity extends AppCompatActivity {
+        public static final String EXTRA_ATTRIBUTE_LIST_1 =
+                "com.example.androidapp.EXTRA_ATTRIBUTE_LIST_1 ";
+        public static final String EXTRA_ATTRIBUTE_LIST_2 =
+                "com.example.androidapp.EXTRA_ATTRIBUTE_LIST_2 ";
+        public static final String EXTRA_ATTRIBUTE_1 =
+                "com.example.androidapp.EXTRA_ATTRIBUTE_1 ";
+        public static final String EXTRA_ATTRIBUTE_2 =
+                "com.example.androidapp.EXTRA_ATTRIBUTE_2 ";
+        public static final String EXTRA_PRODUCT_NAME =
+                "com.example.androidapp.EXTRA_PRODUCT_NAME ";
+        public static final String EXTRA_PRODUCT_TYPE =
+                "com.example.androidapp.EXTRA_PRODUCT_TYPE ";
+
+        private RecyclerView priceQuantityRecycler;
+
+        private Button addBtn;
+        private List<PriceQuantity> priceQuantityList = new ArrayList<>();
+        private Button backBtn;
+        private String productName;
+        private String attribute1;
+        private String attribute2;
+        private String productType;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_quantity_price);
+
+            initUI();
+            //button back
+            backBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    onBackPressed();
+                }
+            });
+            //button add
+            addBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //add data to detail
+                    ProductDetailViewModel productDetailViewModel = new ViewModelProvider(AddQuantityPriceProductActivity.this).get(ProductDetailViewModel.class);
+                    for (PriceQuantity priceQuantity : priceQuantityList) {
+                        for (PriceQuantityItem priceQuantityItem : priceQuantity.getAttribute2()) {
+                            ProductDetail productDetail = new ProductDetail(productName,"", priceQuantity.getAttribute1(),
+                                    priceQuantityItem.getName(), priceQuantityItem.getPrice(),
+                                    priceQuantityItem.getQuantity());
+                            productDetailViewModel.insert(productDetail);
+                        }
+                    }
+                    ProductViewModel productViewModel = new ViewModelProvider(AddQuantityPriceProductActivity.this).get(ProductViewModel.class);
+                    Product product = new Product(productName, "", attribute1, attribute2, productType);
+                    productViewModel.insert(product);
+
+                    setResult(RESULT_OK);
+
+                    onBackPressed();
+                }
+            });
+
+            //get Intent
+            Intent intent = getIntent();
+            productName = intent.getStringExtra(EXTRA_PRODUCT_NAME);
+            productType = intent.getStringExtra(EXTRA_PRODUCT_TYPE);
+            attribute1 = intent.getStringExtra(EXTRA_ATTRIBUTE_1);
+            attribute2 = intent.getStringExtra(EXTRA_ATTRIBUTE_2);
+            List<ProductAttributeItem> lstAttribut1 = intent.getParcelableArrayListExtra(EXTRA_ATTRIBUTE_LIST_1);
+            List<ProductAttributeItem> lstAttribut2 = intent.getParcelableArrayListExtra(EXTRA_ATTRIBUTE_LIST_2);
+
+            //2 attribute
+            if (lstAttribut1 != null && lstAttribut2 != null) {
+                for (int i = 0; i < lstAttribut1.size(); i++) {
+                    PriceQuantity priceQuantity = new PriceQuantity(lstAttribut1.get(i).getAttributeItemName(), new ArrayList<>());
+                    for (int j = 0; j < lstAttribut2.size(); j++) {
+                        priceQuantity.getAttribute2().add(new PriceQuantityItem(lstAttribut2.get(j).getAttributeItemName(), -1, -1));
+                    }
+                    priceQuantityList.add(priceQuantity);
+                }
+            }
+            //1 attribute
+            else if (lstAttribut1 != null && lstAttribut2 == null) {
+                for (int i = 0; i < lstAttribut1.size(); i++) {
+                    PriceQuantity priceQuantity = new PriceQuantity(lstAttribut1.get(i).getAttributeItemName(), new ArrayList<>());
+                    //for all attribute 2
+                    priceQuantity.getAttribute2().add(new PriceQuantityItem("All", -1, -1));
+                    priceQuantityList.add(priceQuantity);
+                }
+            }
+            //0 attribute
+            else {
+                //for all attribute 1
+                PriceQuantity priceQuantity = new PriceQuantity("All", new ArrayList<>());
+                //for all attribute 2
+                priceQuantity.getAttribute2().add(new PriceQuantityItem("All", -1, -1));
+                priceQuantityList.add(priceQuantity);
+            }
+
+            //setup recylcer view
+
+            priceQuantityRecycler.setLayoutManager(new LinearLayoutManager(this));
+            PriceQuantityAdapter priceQuantityAdapter = new PriceQuantityAdapter(priceQuantityList);
+            priceQuantityRecycler.setAdapter(priceQuantityAdapter);
+
+
+        }
+
+
+
+        void initUI() {
+            backBtn = findViewById(R.id.back_btn);
+            priceQuantityRecycler = findViewById(R.id.price_quantity_lst);
+            addBtn = findViewById(R.id.add_product_btn);
+        }
+    }
+
+    public static class ConfirmProductActivity extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_confirm_product);
+        }
+    }
+
+    public static class PickTypeActivity extends AppCompatActivity {
+
+        private Button confirmBtn;
+        private FlexboxLayout lst1;
+        private ProductTypeViewModel productTypeViewModel;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_pick_type);
+
+            //view model
+            productTypeViewModel = new ViewModelProvider(this).get(ProductTypeViewModel.class);
+
+            //init view
+            confirmBtn = findViewById(R.id.confirm_btn);
+            lst1 = findViewById(R.id.lst1);
+
+            confirmBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    checkAllAttribute(lst1);
+                    Intent intent = new Intent(PickTypeActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            });
+
+        }
+        public void checkAllAttribute(FlexboxLayout lst) {
+            for (int i = 0; i < lst.getChildCount(); i++) {
+                final CheckBox attribute = (CheckBox)lst.getChildAt(i);
+                if (attribute.isChecked()) {
+                    ProductType productType = new ProductType(attribute.getText().toString());
+                    productTypeViewModel.insert(productType);
+                }
+            }
+        }
+
+    }
+
+    public static class ProductInfoActivity extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_product_info);
+
+        }
+    }
+
+    public static class QuantityPriceActivity extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_quantity_price);
+        }
+    }
 }
--- AndroidApp/app/src/main/java/com/example/androidapp/HelperClass/NotificationReceiver.java
+++ AndroidApp/app/src/main/java/com/example/androidapp/HelperClass/NotificationReceiver.java
@@ -14,6 +14,7 @@ import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
 import com.example.androidapp.Activities.MainActivity;
+import com.example.androidapp.Activities.SplashActivity;
 import com.example.androidapp.R;
 
 public class NotificationReceiver extends BroadcastReceiver {
@@ -23,18 +24,16 @@ public class NotificationReceiver extends BroadcastReceiver {
         int numOrderTomorrow = intent.getIntExtra("numOrderTomorrow", -1);
         if(numOrderTomorrow > 0) {
 
-//            Intent repeatIntent = new Intent(context, SplashActivity.class);
-//            //Open Upcoming fragment as default
-//            repeatIntent.putExtra("fragmentSelect", 1);
-//            repeatIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            Intent repeatIntent = new Intent(context, SplashActivity.class);
+//
             //Get logo
 //            Bitmap bitmap = BitmapFactory.decodeResource(context.getResources(), R.mipmap.logo);
             //Get sound
             Uri uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
 
-//            PendingIntent pendingIntent = PendingIntent.getActivity(context, 100, repeatIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 100, repeatIntent, PendingIntent.FLAG_UPDATE_CURRENT);
             NotificationCompat.Builder builder = new NotificationCompat.Builder(context, MainActivity.CHANNEL_ID)
-//                    .setContentIntent(pendingIntent)
+                    .setContentIntent(pendingIntent)
 //                    .setSmallIcon(R.drawable.logo_small)
 //                    .setLargeIcon(bitmap)
                     .setSound(uri)
